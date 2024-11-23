fe1bd8309296796b1c7a699daf4a49294e7c4118
Ryan-RH <treezysmurf6@gmail.com>
2024-11-23 19:39:23 +0000

    a

--- MountGuide/MountGuide.cs
+++ MountGuide/MountGuide.cs
@@ -5,45 +5,22 @@ using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
 using MountGuide.Windows;
+using ECommons;
 
 namespace MountGuide;
 
 public sealed class MountGuide : IDalamudPlugin
 {
-    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
-    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
-
-    private const string CommandName = "/pmycommand";
-    public Configuration Configuration { get; init; }
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
-    private MainWindow MainWindow { get; init; }
-
+    internal static MountGuide P;
     public MountGuide()
     {
-        Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
-
-        var goatImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
-
-        MainWindow = new MainWindow(this, goatImagePath);
-
-        WindowSystem.AddWindow(MainWindow);
-
-        PluginInterface.UiBuilder.Draw += DrawUI;
-
-        PluginInterface.UiBuilder.OpenMainUi += ToggleMainUI;
+        P = this;
+        ECommonsMain
     }
 
     public void Dispose()
     {
-        WindowSystem.RemoveAllWindows();
-
-        MainWindow.Dispose();
-
-        CommandManager.RemoveHandler(CommandName);
+        
     }
 
-    private void DrawUI() => WindowSystem.Draw();
-
-    public void ToggleMainUI() => MainWindow.Toggle();
 }
